909ac630237c753b4899401177a0f8e8c4f00535
Nestor <nestor_escoto@hotmail.com>
2017-09-11 22:48:25 -0600

    Testing out, ready for API

--- model_analysis/model_training.py
+++ model_analysis/model_training.py
@@ -2,10 +2,12 @@ from sae import SAE
 import torch
 
 training_set = torch.load('./training_set.pkl')
+test_set = torch.load('./test_set.pkl')
 sae = SAE(3787, encoder_input=40, decoder_input=40)
 sae.add_hiden_layer(20)
 sae.add_dropout(0.2)
 sae.add_hiden_layer(40)
 sae.compile(optimizer='adam')
-sae.fit(training_set, 200)
+sae.fit(training_set, 5)
+sae.perform(training_set, test_set)
 torch.save(sae, 'model.pkl')
